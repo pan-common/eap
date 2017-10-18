4e007e2650482906e353c076fba1b8d6fd4535f1
panho <panhongzhi02@163.com>
2017-10-18 23:01:31 +0800

    1、2017年10月18日 解决了select赋值问题，蛋疼的layui，浪费了一晚上时间

---
+++ src/main/java/com/taiji/eap/common/taglib/CommonSelectTag.java
@@ -0,0 +1,14 @@
+package com.taiji.eap.common.taglib;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.servlet.jsp.tagext.TagSupport;
+
+/**
+ * 从所有数据源中进行选择
+ */
+public class CommonSelectTag extends TagSupport{
+
+
+
+}
---
+++ src/main/java/com/taiji/eap/common/taglib/select/SelectDataSource.java
@@ -0,0 +1,12 @@
+package com.taiji.eap.common.taglib.select;
+
+import java.util.List;
+
+public abstract class SelectDataSource {
+
+    public abstract String getKeyName();
+
+    public abstract String getValueName();
+
+
+}
